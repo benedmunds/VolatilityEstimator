8141475bfa60c83e4ed0229515c879ed2c1cc26e
Ben Edmunds <ben.edmunds@gmail.com>
2018-07-11 10:22:37 -0400

    Working on readme markdown formatting

--- README.md
+++ README.md
@@ -3,17 +3,11 @@ Implementation of various volatility calculations in Javascript.
 
 Data represents the E-mini S&P 500 Futures
 
-# Latest Calculation 
-
-Instrument: ES 
+# Latest Calculations
 
 Method: Yang-Zhang 
 
 Notes: Implementation of http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.628.4037&rep=rep1&type=pdf 
-
-Latest Data: 7/10/2018 
-
-Periods: 20 
 
 | Meta        |            | Stat          | Value    |
 | ----------- | ---------- | ------------- |:--------:|
--- index.js
+++ index.js
@@ -33,17 +33,11 @@ Implementation of various volatility calculations in Javascript.
 
 Data represents the E-mini S&P 500 Futures
 
-# Latest Calculation 
-
-Instrument: ES 
+# Latest Calculations
 
 Method: Yang-Zhang 
 
 Notes: Implementation of http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.628.4037&rep=rep1&type=pdf 
-
-Latest Data: ${data[0].date} 
-
-Periods: ${n} 
 
 | Meta        |            | Stat          | Value    |
 | ----------- | ---------- | ------------- |:--------:|
