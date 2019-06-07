9dc016e0c7b988372484313439fdd9a386fb238d
Ben Edmunds <ben.edmunds@gmail.com>
2019-06-07 12:51:43 -0400

    Added ATR calculation - simplified and added yesterday to calc

--- models/atr.js
+++ models/atr.js
@@ -10,25 +10,23 @@ module.exports = function(n, data){
 
         for (let i=0; i<n; i++) {
 
-          // First Record
+          // First Record - Just set it based on this days data
           if (i === 0) {
             results.push({trueRange:data[i]['high'] - data[i]['low'], atr:0});
             continue;
           }
 
-          let atr = 0;
+          // Calculate true range for the day
           const range = data[i]['high'] - data[i]['low'];
           const closeToHigh = Math.abs(data[i]['high'] - data[i-1]['close']);
           const closeToLow = Math.abs(data[i]['low'] - data[i-1]['close']);
           const trueRange = Math.max(range, closeToHigh, closeToLow);
+          results.push({trueRange:trueRange, date: data[i]['date']});
 
-          // Last Record
+          // Last Record - Average and Return ATR
           if (i === (n-1)) {
             return results.reduce((accumulator, val) => accumulator + val.trueRange, trueRange) / n;
           }
-
-          atr = ((results[i-1].atr * (n-1) + trueRange) / n);
-          results.push({trueRange:trueRange, atr:atr, date: data[i]['date']});
 
         }
 
